c79e11c1cfea9d23ad39ee5cd11d9eecd37ab68c
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2021-10-12 19:31:30 -0300

    Adionando SQL e iniciando backend com Node

--- api/db/script.sql
+++ api/db/script.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE IF NOT EXISTS cart;
-
 USE cart;
 
-CREATE TABLE IF NOT EXISTS product (
-    id INT(11) AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS products (
+    id INT(11) AUTO_INCREMENT,
     name VARCHAR(255),
     price DECIMAL(10, 2),
     PRIMARY KEY (id)
 );
 
-INSERT INTO product (name, price) VALUES ('Product-1', 1.50);
+INSERT INTO products VALUE(0, 'Product-1', 150);
 
-INSERT INTO product (name, price) VALUES ('Product-2', 2);
+INSERT INTO products VALUE(0, 'Product-2', 2);
+
